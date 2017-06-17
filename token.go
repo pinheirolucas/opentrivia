0495a0743c1b0508615de6c96fe081752d717a9f
Lucas Pinheiro <lucas.contato1996@gmail.com>
2017-06-16 21:05:55 -0300

    token errors

--- token.go
+++ token.go
@@ -1,13 +1,23 @@
 package opentrivia
 
+import (
+	"errors"
+)
+
 // Token is the type for tokens.
 type Token string
 
-// TokenEmptyError is returned when the Open Trivia API has
-// returned all possible questions for the specified query.
-//
-// Resetting the Token is necessary to keep on running.
-type TokenEmptyError error
+var (
+	// ErrTokenEmpty is returned when the Open Trivia API has
+	// returned all possible questions for the specified query.
+	//
+	// Resetting the Token is necessary to keep on running.
+	ErrTokenEmpty = errors.New("opentrivia: token has returned all possible questions for the specified query")
+
+	// ErrTokenNotFound is returned when the Open Trivia API
+	// do not found the provided token.
+	ErrTokenNotFound = errors.New("opentrivia: token does not exist")
+)
 
 // TokenService handles communication with the token related
 // methods of the Open Trivia API
@@ -23,7 +33,7 @@ type TokenService service
 // the same question twice.
 //
 // If all questions for a given category has already been returned,
-// the request will return an opentrivia.TokenEmptyError.
+// the request will return an opentrivia.ErrTokenEmpty.
 func (t *TokenService) Create() (Token, error) {
 	return "", nil
 }
@@ -31,7 +41,7 @@ func (t *TokenService) Create() (Token, error) {
 // Reset refresh the provided token.
 //
 // If the provided token is invalid, the request will return an
-// opentrivia.TokenNotFoundError.
+// opentrivia.ErrTokenNotFound.
 func (t *TokenService) Reset(token Token) (Token, error) {
 	return "", nil
 }
@@ -39,7 +49,7 @@ func (t *TokenService) Reset(token Token) (Token, error) {
 // StillValid should be used to check if the token still valid.
 //
 // If the provided token is not found, the request will return an
-// opentrivia.TokenNotFoundError.
+// opentrivia.ErrTokenNotFound.
 func (t *TokenService) StillValid(token Token) (bool, error) {
 	return false, nil
 }
